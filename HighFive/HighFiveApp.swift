db3dee70a474109a2e6cdae168a691692b5d2c4b
Michael Alston <michael@local>
2026-04-16 14:00:51 -0600

    Initial Commit

---
+++ HighFive/HighFiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighFiveApp.swift
+//  HighFive
+//
+//  Created by Michael Alston on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct HighFiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
